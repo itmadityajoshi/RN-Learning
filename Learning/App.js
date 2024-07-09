361eb98c645d6905a34ea1d35beea217513cc900
itmadityajoshi <adityajoshi316@gmail.com>
2024-07-09 22:19:58 +0545

    separate components are added on app

--- Learning/App.js
+++ Learning/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {View, Button, Text} from 'react-native'
+import ButtonEvent from './components/ButtonEvent'
+import StateinRN from './components/StateinRN';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+const App =()=>{
+  return(
+    <View>
+      <Text> Hello </Text>
+     {/* <ButtonEvent/> */}
+     <StateinRN />
+   
     </View>
-  );
+  ) 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
