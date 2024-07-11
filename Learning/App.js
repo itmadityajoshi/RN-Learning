2892156f917d422be70c5a8ab356b6b25100a3c3
itmadityajoshi <adityajoshi316@gmail.com>
2024-07-11 22:37:00 +0545

    added more components to the main app bundle

--- Learning/App.js
+++ Learning/App.js
@@ -1,17 +1,27 @@
-import React from 'react';
-import {View, Button, Text} from 'react-native'
-import ButtonEvent from './components/ButtonEvent'
-import StateinRN from './components/StateinRN';
+import React from "react";
+import { View, Button, Text, SafeAreaView } from "react-native";
+import ButtonEvent from "./components/ButtonEvent";
+import StateinRN from "./components/StateinRN";
+import PropsRN from "./components/PropsRN";
+import Styling from "./components/Styling";
+import HandletextInput from "./components/HandletextInput";
+import SimpleForm from "./components/SimpleForm";
+import Flatlist from "./components/Flatlist";
 
-
-const App =()=>{
-  return(
-    <View>
-      <Text> Hello </Text>
-     {/* <ButtonEvent/> */}
-     <StateinRN />
-   
-    </View>
-  ) 
-}
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text> Hello </Text>
+        {/* <ButtonEvent/> */}
+        {/* <StateinRN /> */}
+        {/* <PropsRN /> */}
+        {/* <Styling/>  */}
+        {/* <HandletextInput/> */}
+        {/* <SimpleForm/> */}
+        <Flatlist/>
+      </View>
+    </SafeAreaView>
+  );
+};
 export default App;
