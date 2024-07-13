96e368a2135e85e04b6e8e40866bca1e10121bfd
itmadityajoshi <adityajoshi316@gmail.com>
2024-07-13 22:02:13 +0545

    use useEffect with componentdid Update

---
+++ Learning/components/UseEffecth.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { Button, Text, View } from "react-native";
+
+const UseEffecth = () => {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(100);
+
+  useEffect(() => {
+    console.warn("Error data");
+  }, [count,data]);
+  return (
+    <View>
+      <Text> useEffect Hook life cycle</Text>
+      <Text style={{fontSize:19}}>{count}</Text>
+      <Button title="update Count" onPress={() => setCount(count + 1)} />
+      <Text style={{fontSize:19}}>{data}</Text>
+
+      <Button title="update Data" onPress={() => setData(data + 1)} />
+    </View>
+  );
+};
+
+export default UseEffecth;
